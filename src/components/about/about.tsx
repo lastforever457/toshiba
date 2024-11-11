9bf1988b200a1c4bc1f21682e766eca1e9856656
lastforever457 <lastforever457@gmail.com>
2024-11-11 23:29:18 +0500

    fix

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,42 +1,43 @@
 import { Col, Row } from "antd";
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
 
 const About = () => {
+  const { t } = useTranslation();
+  const items = useMemo(
+    () => [
+      {
+        label: t("sifat"),
+        content: t("sifat desc"),
+      },
+      {
+        label: t("security"),
+        content: t("security desc"),
+      },
+      {
+        label: t("tech support"),
+        content: t("tech support desc"),
+      },
+    ],
+    [t]
+  );
   return (
-    <div
-      id="about"
-      className="w-full min-h-[400px] px-5 mdpx-10 xl:px-72 py-16"
-    >
-      <div
-        className="flex min-h-[400px] bg-center rounded-xl overflow-hidden md:rounded-none bg-cover object-cover"
-        style={{ backgroundImage: `url("/about.png")` }}
-      >
-        <div
-          className="flex w-full h-full"
-          style={{ backdropFilter: "blur(5px)" }}
-        >
-          <Row>
-            <Col xs={24} sm={24} md={24} lg={12}></Col>
-            <Col xs={24} sm={24} md={24} lg={12}>
-              <div className="flex flex-col justify-center items-start text-white p-5 gap-2 w-full h-full">
-                <h2 className="text-3xl font-bold">
-                  Современный ремонт и отделка квартир Под Ключ - USTAZODA.UZ
-                </h2>
-                <p className="text-xl">
-                  Современный ремонт и отделка квартир Под Ключ в Ташкенте и по
-                  всему Узбекистану. Ремонт квартир, домов и других, жилых
-                  помещений любой сложности под ключ форма оплаты любая,
-                  Ламинат, пластик, линолеум, ковролан, гипсокартон любой
-                  сложности, укладка кафеля, мрамор, гранит, корка, цоколь,
-                  малярь-ка, обои, штукатурка, декор, электросварка, сантехника,
-                  электрика, установка козырьков, установка дверей топчаны,
-                  стяжка пола, кладка кирпичей, установка ванн и сантехники,
-                  сборка и разборка мебели.
-                </p>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </div>
+    <div className="px-5 md:px-10 xl:px-72 py-16" id="about">
+      <h1 className="text-center text-3xl mb-5 uppercase">{t("about")}</h1>
+      <p className="text-center text-lg mb-10">{t("about slogan")}</p>
+      <Row gutter={[16, 16]}>
+        {items.map((item, index) => (
+          <Col key={index} xs={24} sm={12} md={8} lg={8} xl={8}>
+            <div
+              className="flex flex-col h-full justify-center gap-2 items-center px-5 py-10 text-white cursor-pointer bg-center bg-cover bg-no-repeat rounded-xl object-cover"
+              style={{ backgroundImage: `url("/card-bg.png")` }}
+            >
+              <h2 className="text-2xl font-bold">{item.label}</h2>
+              <p className="text-center text-lg">{item.content}</p>
+            </div>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
